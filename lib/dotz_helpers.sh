8013ac5a356bd0fcbfd9095f5c7cc465960120b6
ftc2 <ftc2@users.noreply.github.com>
2020-09-28 21:32:40 -0400

    dotz_helpers.sh: updated brew install command

--- lib/dotz_helpers.sh
+++ lib/dotz_helpers.sh
@@ -4,7 +4,7 @@
 dotz_homebrew_install() {
   command -v brew >/dev/null || {
     echo 'Homebrew not found! Installing Homebrew...'
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   }
 }
 
